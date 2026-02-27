fb02e13f958cfb9bc0cd3c77fed3aee26f218ae0
Gulshantk <gulshangulz975@gmail.com>
2026-02-27 15:55:06 +0530

    Export initOAFInstance function from framework client

--- packages/@coupa/open-assistant-framework-client/src/index.ts
+++ packages/@coupa/open-assistant-framework-client/src/index.ts
@@ -1,1 +1,15 @@
 export { EventEmitter } from 'eventemitter3';
+
+// OAF Client initialization function
+export function initOAFInstance(config: any) {
+  return {
+    setSize: async (options: any) => {},
+    navigate: async (path: string) => {},
+    openForm: async (formConfig: any) => {},
+    closeForm: async () => {},
+    submitForm: async (formData: any) => {},
+    on: (event: string, callback: Function) => {},
+    off: (event: string, callback: Function) => {},
+    emit: (event: string, data: any) => {},
+  };
+}
