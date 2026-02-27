f8451140486f245aff26b76c6435f485f0875820
Gulshantk <gulshangulz975@gmail.com>
2026-02-27 15:57:42 +0530

    Use void references to silence unused variable errors

--- packages/@coupa/open-assistant-framework-client/src/index.ts
+++ packages/@coupa/open-assistant-framework-client/src/index.ts
@@ -2,14 +2,16 @@ export { EventEmitter } from 'eventemitter3';
 
 // OAF Client initialization function
 export function initOAFInstance(config: any) {
+  // avoid unused-variable errors by referencing parameters
+  void config;
   return {
-    setSize: async (options: any) => {},
-    navigate: async (path: string) => {},
-    openForm: async (formConfig: any) => {},
+    setSize: async (options: any) => { void options; },
+    navigate: async (path: string) => { void path; },
+    openForm: async (formConfig: any) => { void formConfig; },
     closeForm: async () => {},
-    submitForm: async (formData: any) => {},
-    on: (event: string, callback: Function) => {},
-    off: (event: string, callback: Function) => {},
-    emit: (event: string, data: any) => {},
+    submitForm: async (formData: any) => { void formData; },
+    on: (event: string, callback: Function) => { void event; void callback; },
+    off: (event: string, callback: Function) => { void event; void callback; },
+    emit: (event: string, data: any) => { void event; void data; },
   };
 }
