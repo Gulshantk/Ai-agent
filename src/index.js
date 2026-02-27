b2a9204501f196b182a8c19ed29dcd9fa08cafee
Gulshantk <gulshangulz975@gmail.com>
2026-02-27 15:46:14 +0530

    Update changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import '../styles/index.css';
-import Main from './app/main';
-
+import Main from './app/main.jsx';
 createRoot(document.getElementById('root')).render(React.createElement(Main));
