e63de845639872208fd2972bbc04a7cf2c293f1d
Gulshantk <gulshangulz975@gmail.com>
2026-02-27 15:47:51 +0530

    Fix import path: main.jsx to Main.jsx

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import '../styles/index.css';
-import Main from './app/main.jsx';
+import Main from './app/Main.jsx';
 createRoot(document.getElementById('root')).render(React.createElement(Main));
